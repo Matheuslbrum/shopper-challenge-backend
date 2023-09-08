598446ff3c3af8b4a872695a8c07246942908ff5
MatheusLBrum <matheusdelimabrum@gmail.com>
2023-09-08 16:11:02 -0300

    config upload and db lib

---
+++ src/config/dbConfig.js
@@ -0,0 +1,10 @@
+import mysql from 'mysql2';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export const connection = mysql.createConnection({
+  host: process.env.DB_LOCALHOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
