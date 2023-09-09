dc2014c0e96293b0f0a42a0b7445fcfacceccb9d
MatheusLBrum <matheusdelimabrum@gmail.com>
2023-09-08 22:45:33 -0300

    adding router, cors, dotenv in index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import router from './src/routes/validateRoute.js'; 
+import cors from 'cors'
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 const app = express();
+app.use(cors())
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(router)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
